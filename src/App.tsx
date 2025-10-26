475ac470fedfae94630e6a08a45552524f9dabf7
Alex Kostyniuk <kostyniuk7@gmail.com>
2025-10-26 11:09:40 +0100

    make it controlled

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import { useState } from 'react';
 import { useSortable } from '@dnd-kit/react/sortable';
+import { DragDropProvider } from '@dnd-kit/react';
+import { move } from '@dnd-kit/helpers';
 import './index.css';
 
-function Item({ id, index, column }: { id: string, index: number, column: string }) {
-  const { ref, isDragging } = useSortable({
-    id,
-    index,
-    type: 'item',
-    accept: 'item',
-    group: column,
-  });
+function Item({ id, index }: { id: string, index: number }) {
+  const { ref, isDragging } = useSortable({ id, index });
   return (
     <button className="Item" ref={ref} data-dragging={isDragging}>
       {id}
@@ -18,16 +14,31 @@ function Item({ id, index, column }: { id: string, index: number, column: string
 }
 
 export default function App() {
-  const [itemsFlat] = useState<string[]>(['first', 'second', 'third', 'fourth']);
-
+  const [items, setItems] = useState<string[]>(['first', 'second', 'third', 'fourth']);
   return (
-    <div className="Root">
-      <div key="Flat" className="Column">
-        {itemsFlat.map((id, index) => (
-          <Item key={id} id={id} index={index} column="Flat" />
+    <DragDropProvider onDragOver={(event) => {
+      setItems((items) => move(items, event));
+    }}>
+      <div className="Root">
+        <div key="Flat" className="Column">
+          {items.map((id, index) => (
+            <Item key={id} id={id} index={index} />
+          ))}
+        </div>
+      </div>
+      <div>
+        <tr>
+          <th>Index</th>
+          <th>ID</th>
+        </tr>
+        {items.map((id, index) => (
+          <tr key={index}>
+            <td>{index}</td>
+            <td>{id}</td>
+          </tr>
         ))}
       </div>
-    </div>
+    </DragDropProvider>
 
   );
 }
